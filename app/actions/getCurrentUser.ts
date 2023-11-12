d5d1be2cf7ad60ca43c3754713ee92955a8727a3
lewis <lewiswalter192@gmail.com>
2023-11-13 00:05:30 +0300

    second commit

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,32 +1,31 @@
-import { getServerSession } from "next-auth";
-import { handler } from "../api/auth/[...nextauth]/route";
-import prisma from '@/app/lib/prismadb'
+// import { getServerSession } from "next-auth";
+// import prisma from '@/app/lib/prismadb'
 
-export async function getSession() {
-    return await getServerSession(handler)
-}
+// export async function getSession() {
+//     return await getServerSession()
+// }
 
 
-export default async function getCurrentUser(){
-    try {
-        const session = await getSession();
+// export default async function getCurrentUser(){
+//     try {
+//         const session = await getSession();
 
-        if(!session?.user?.email){
-            return null;
-        }
+//         if(!session?.user?.email){
+//             return null;
+//         }
     
-        const user = await prisma.user.findUnique({
-            where: {
-                email: session.user.email
-            }
-        });
+//         const user = await prisma.user.findUnique({
+//             where: {
+//                 email: session.user.email
+//             }
+//         });
     
-        if(!user) {
-            return null;
-        }
+//         if(!user) {
+//             return null;
+//         }
     
-        return user;
-    } catch (error) {
-        return null;
-    }
-}
+//         return user;
+//     } catch (error) {
+//         return null;
+//     }
+// }
