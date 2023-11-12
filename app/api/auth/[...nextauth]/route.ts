f66efc8f10b14aefe093d05c56421e5a451830cb
lewis <lewiswalter192@gmail.com>
2023-11-13 00:54:45 +0300

    second commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,124 +1,62 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import bcrypt from 'bcrypt'
-import { AuthOptions } from 'next-auth'
-import NextAuth from 'next-auth/next'
-import FaceBookProvider from 'next-auth/providers/facebook'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import prisma from '@/app/lib/prismadb'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
+import NextAuth from "next-auth/next";
+import FaceBookProvider from "next-auth/providers/facebook";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/app/lib/prismadb";
 
-
-export async function GET(): Promise<AuthOptions> {
-     const handler: AuthOptions = NextAuth({
-        adapter: PrismaAdapter(prisma),
-        providers: [
-            FaceBookProvider({
-                clientId: process.env.FACEBOOK_ID as string,
-                clientSecret: process.env.FACEBOOK_SECRET as string
-            }),
-            CredentialsProvider({
-                name: 'credentials',
-                credentials: {
-                    email: {
-                        label: 'text',
-                        type: 'text',
-                    },
-                    password: {
-                        label: 'password',
-                        type: 'password'
-                    }
-                },
-                async authorize(credentials) {
-                    if (!credentials?.password || !credentials?.email) {
-                        throw new Error('Invalid Credentials');
-                    }
-
-                    const user = await prisma.user.findUnique({
-                        where: {
-                            email: credentials.email
-                        }
-                    });
-
-                    if (!user || !user.hashedPassword) {
-                        throw new Error('Invalid Credentials');
-                    }
-
-                    const isCorrectPassword = await bcrypt.compare(
-                        credentials.password, user?.hashedPassword
-                    );
-
-                    if (!isCorrectPassword) {
-                        throw new Error('Invalid Credentials');
-                    }
-
-                    return user;
-                }
-            })
-        ],
-        debug: process.env.NODE_ENV === 'development',
-        session: {
-            strategy: 'jwt'
+export const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    FaceBookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "text",
+          type: "text",
         },
-        secret: process.env.NEXTAUTH_SECRET
-    });
+        password: {
+          label: "password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.password || !credentials?.email) {
+          throw new Error("Invalid Credentials");
+        }
 
-    return handler;
-}
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid Credentails");
+        }
 
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user?.hashedPassword
+        );
 
+        if (!isCorrectPassword) {
+          throw new Error("Invalid Credentials");
+        }
 
-// export const handler: AuthOptions = NextAuth({
-//     adapter: PrismaAdapter(prisma),
-//     providers: [
-//         FaceBookProvder({
-//             clientId: process.env.FACEBOOK_ID as string,
-//             clientSecret: process.env.FACEBOOK_SECRET as string
-//         }),
-//         CredentialsProvider({
-//             name: 'credentials',
-//             credentials: {
-//                 email: {
-//                     label: 'text',
-//                     type: "text",
-//                 },
-//                 password: {
-//                     label: 'password',
-//                     type: 'password'
-//                 }
-//             },
-//             async authorize(credentials){
-//                 if(!credentials?.password || !credentials?.email){
-//                     throw new Error("Invalid Credentials");
-//                 }
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
-//                 const user = await prisma.user.findUnique({
-//                     where: {
-//                         email: credentials.email
-//                     }
-//                 });
-
-//                 if(!user || !user.hashedPassword){
-//                     throw new Error("Invalid Credentails");       
-//                 }
-
-//                 const isCorrectPassword = await bcrypt.compare(
-//                     credentials.password, user?.hashedPassword
-//                 )
-                
-//                 if(!isCorrectPassword){
-//                     throw new Error("Invalid Credentials");                
-//                 }
-
-//                 return user;
-
-//             }
-//         })
-//     ],
-//     debug: process.env.NODE_ENV === 'development',
-//     session: {
-//         strategy: 'jwt'
-//     },
-//     secret: process.env.NEXTAUTH_SECRET
-// })
-
-// export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
