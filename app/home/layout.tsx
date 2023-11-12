715786d3f422105ec3d3297108cd31987a684240
lewis <lewiswalter192@gmail.com>
2023-11-12 22:59:57 +0300

    second commit

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,19 +1,14 @@
-import getCurrentUser from "../actions/getCurrentUser";
-
+import Sidebar from "./components/Sidebar"
 
 export default async function RootLayout({
     children
 }: {children: React.ReactNode}){
 
-    const currentUser = await getCurrentUser()
-
-    if(!currentUser) {
-        return null
-    }
 
     return (
         <body>
             <div className="flex">
+                <Sidebar />
                 <div className="container min-h-screen bg-black text-white">
                     {children}
                </div>
